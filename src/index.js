5c8cbd50fdc0fe8e9090974c8d1b1a38041dbb4c
qmpzqmpz <hyj5579@gmail.com>
2020-05-14 18:33:48 +0900

    add authenticator values
    add authenticator values for Builder#setAllowedAuthenticators(int)
    see [https://developer.android.com/reference/kotlin/android/hardware/biometrics/BiometricManager.Authenticators?hl=ko] and [https://developer.android.com/reference/kotlin/android/hardware/biometrics/BiometricPrompt.Builder?hl=ko#setAllowedAuthenticators(kotlin.Int)] for detail information.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import authenticate from './authenticate';
 import isSensorAvailable from './isSensorAvailable';
 import release from './release';
 
+const BIOMETRIC_STRONG = 15;
+const BIOMETRIC_WEAK = 255;
+const DEVICE_CREDENTIAL = 32768;
+
 export default {
   authenticate,
   release,
   isSensorAvailable,
+  BIOMETRIC_STRING,
+  BIOMETRIC_WEAK,
+  DEVICE_CREDENTIAL,
 };
